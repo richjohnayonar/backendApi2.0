8c601957e3d5391e5649826dcaeb2412b1981f30
richjohnayonar <richjohnayonar@gmail.com>
2023-10-09 15:53:38 +0800

    added model to carousel table and create announcement migrations

---
+++ app/Models/carouselItems.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class carouselItems extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'announcement';
+    
+     /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'carousel_item_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'carousel_name',
+        'image_path',
+        'description',
+        'user_id',
+];  
+}
