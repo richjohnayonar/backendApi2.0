3a99ef68c36c9b62bf2b4697407e9184c5519f50
richjohnayonar <richjohnayonar@gmail.com>
2023-10-09 17:29:06 +0800

    add carousel controller, seed data to user table, and add routes to show all data from announcement table

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\carouselItemsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+  
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/carousel', [carouselItemsController::class, 'index']);
